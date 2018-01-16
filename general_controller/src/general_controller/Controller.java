53d2f47a2d79315cb4b3cb74fc57f16090c687d7
Grawta <nabalblanc@laposte.net>
2018-01-16 17:53:48 +0100

    getFlowInfo and getFlow

--- general_controller/src/general_controller/Controller.java
+++ general_controller/src/general_controller/Controller.java
@@ -2,4 +2,7 @@ package general_controller;
 
 public class Controller {
 
+	public String getFlow(String switchID, String portID) {
+		return SDNControllerAdapter.getFlowInfo(switchID, portID);
+	}
 }
--- general_controller/src/general_controller/SDNControllerAdapter.java
+++ general_controller/src/general_controller/SDNControllerAdapter.java
@@ -3,34 +3,37 @@ package general_controller;
 import org.json.simple.JSONObject;
 
 public class SDNControllerAdapter {
-	
+
 	/*
 	 * This method permits to create new roads in json format
-	 * */
+	 */
 	private static JSONObject createRoute(String ipSrc, String ipDst, String ethDst, String ethSrc) {
 		String switchID = "00:00:00:00:00:00:00:02";
-		String actionMan = "set_field=eth_dst->"+ethDst+",set_field=ipv4_dst->"+ipDst+",normal";
+		String actionMan = "set_field=eth_dst->" + ethDst + ",set_field=ipv4_dst->" + ipDst + ",normal";
 		JSONObject json = null;
 		json.put("switch", switchID);
 		json.put("name", "ZeName");
-		json.put("priority","36000");
-		json.put("eth_type","0x0800");
-		json.put("eth_src",ethSrc);
-		json.put("ipv4_src",ipSrc);
-		json.put("eth_dst","00:00:00:00:00:05");
-		json.put("ipv4_dst","10.0.0.3");
-		json.put("actions",actionMan);
+		json.put("priority", "36000");
+		json.put("eth_type", "0x0800");
+		json.put("eth_src", ethSrc);
+		json.put("ipv4_src", ipSrc);
+		json.put("eth_dst", "00:00:00:00:00:05");
+		json.put("ipv4_dst", "10.0.0.3");
+		json.put("actions", actionMan);
 		return json;
 	}
 
-	
 	/*
-	 *	This method permits to updload new roads to a switch  
+	 * This method permits to updload new roads to a switch
+	 */
+	public static void reRoute(String ipSrc, String ipDst, String ethDst, String ethSrc) {
+		RestClient.post("http://10.0.2.15:8080/wm/staticflowpusher/json", createRoute(ipSrc, ipDst, ethDst, ethSrc));
+	}
+
+	/*
+	 * This method permits to get flow from a switch and a portID
 	 * */
-	public static void reRoute(String ipSrc, String ipDst, String ethDst, String ethSrc) {	
-		RestClient.post("http://10.0.2.15:8080/wm/staticflowpusher/json", createRoute(ipSrc,ipDst,ethDst,ethSrc));
+	public static String getFlowInfo(String switchID, String portID) {
+		return RestClient.get("http://10.0.2.15:8080/wm/statistics/bandwidth/" + switchID + "/" + portID + "/json");
 	}
 }
-	
-	
-	
