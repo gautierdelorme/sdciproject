4861b5eb3fb2bde85933b9613daf8fed48c16cd5
Grawta <nabalblanc@laposte.net>
2018-01-21 15:46:53 +0100

    Add auto mode and disauto mode

--- general_controller/src/general_controller/Controller.java
+++ general_controller/src/general_controller/Controller.java
@@ -33,7 +33,7 @@ public class Controller {
 		serv.get("/trigger/:name", ctx -> {
 			int portCont = VNFManager.launchGW(ctx.param("name"));
 			SDNControllerAdapter.reRoute("10.0.0.4", "00:00:00:00:00:02", portCont,ctx.param("name"));
-			ctx.result("Well done you've redirected GF1 to your new gateway, Nane of the gateway : "+ ctx.param("name")+ "\n");
+			ctx.result("Well done you've redirected GF1 to your new gateway, Name of the gateway : "+ ctx.param("name")+ "\n");
 		});
 		
 		serv.delete("rules/delete", ctx -> {
@@ -42,10 +42,30 @@ public class Controller {
 			ctx.result("You have deleted the rule : "+ jsonBody.get("name") + "\n");
 		});
 		
-		serv.post("rules/delete/:ruleName", ctx -> {
+		
+		/*
+		 * Dans notre cas la regle sera renomme du nom de la nouvelle GW pour des questions de praticites
+		 * */
+		serv.delete("rules/delete/:ruleName", ctx -> {
+			SDNControllerAdapter.deleteRoute(ctx.param("ruleName"));
 			ctx.result("You have deleted the rule : "+ ctx.param("ruleName") + "\n");
 		});
 		
+		/*
+		 * Start the thread and make automatic flow gestion
+		 * */
+		serv.get("/mode-auto-on",  ctx ->{
+			FlowGestion.start1();
+			ctx.result("You set auto-mode on ! \n");
+		});
+		
+		/*
+		 * Stop the thread
+		 */
+		serv.get("/mode-auto-off", ctx ->{
+			FlowGestion.interrupted();
+			ctx.result("You set auto-mode off ! \n");
+		});
 
 	}
 }
--- general_controller/src/general_controller/FlowGestion.java
+++ general_controller/src/general_controller/FlowGestion.java
@@ -1,22 +1,27 @@
 package general_controller;
 
 public class FlowGestion extends Thread {
-	String flow;
+	private volatile String flow;
 
 	public FlowGestion() {
 		this.flow = null;
 	}
 
 	public void run() {
-		while (true) {
+		while (!isInterrupted()) {
 			this.flow = SDNControllerAdapter.getFlowInfo("00:00:00:00:00:00:00:02", "2");
 			System.out.println("Print flow on port 2 main switch");
 			System.out.println(flow);
 			try {
 				Thread.sleep(10000);
 			} catch (InterruptedException e) {
-				e.printStackTrace();
+				System.out.println("Flow gestion interrupted mode auto : OFF");
+				Thread.currentThread().interrupt();
 			}
 		}
+	}
+	public static void start1() {
+		FlowGestion flowG = new FlowGestion();
+		flowG.start();
 	}
 }
