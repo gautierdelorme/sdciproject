a4fca80ad24cbb5b20a86e46a745f4eb64c30098
Grawta <nabalblanc@laposte.net>
2018-01-21 15:56:01 +0100

    Changing name GW generation

--- general_controller/src/general_controller/Controller.java
+++ general_controller/src/general_controller/Controller.java
@@ -1,7 +1,9 @@
 package general_controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.log4j.BasicConfigurator;
-import org.json.simple.JSONObject;
 
 import io.javalin.Javalin;
 
@@ -25,44 +27,39 @@ public class Controller {
 		serv.get("/hello", ctx -> {
 			ctx.result("Hello \n");
 		});
-		
-		
+
 		/*
-		 * This line is call with a get /trigger/"name of the new container" it will create it and then reroute GF1 to this new container instead of GI
-		 * */
-		serv.get("/trigger/:name", ctx -> {
-			int portCont = VNFManager.launchGW(ctx.param("name"));
-			SDNControllerAdapter.reRoute("10.0.0.4", "00:00:00:00:00:02", portCont,ctx.param("name"));
-			ctx.result("Well done you've redirected GF1 to your new gateway, Name of the gateway : "+ ctx.param("name")+ "\n");
+		 * This line is call with a get /trigger/"name of the new container" it will
+		 * create it and then reroute GF1 to this new container instead of GI
+		 */
+		serv.get("/trigger", ctx -> {
+			String name = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+			int portCont = VNFManager.launchGW(name);
+			SDNControllerAdapter.reRoute("10.0.0.4", "00:00:00:00:00:02", portCont, name);
+			ctx.result("Well done you've redirected GF1 to your new gateway, Name of the gateway : " + name + "\n");
 		});
-		
-		serv.delete("rules/delete", ctx -> {
-			JSONObject jsonBody = ctx.bodyAsClass(JSONObject.class);
-			SDNControllerAdapter.deleteRoute((String)jsonBody.get("name"));
-			ctx.result("You have deleted the rule : "+ jsonBody.get("name") + "\n");
-		});
-		
-		
+
 		/*
-		 * Dans notre cas la regle sera renomme du nom de la nouvelle GW pour des questions de praticites
-		 * */
+		 * Dans notre cas la regle sera renomme du nom de la nouvelle GW pour des
+		 * questions de praticites
+		 */
 		serv.delete("rules/delete/:ruleName", ctx -> {
 			SDNControllerAdapter.deleteRoute(ctx.param("ruleName"));
-			ctx.result("You have deleted the rule : "+ ctx.param("ruleName") + "\n");
+			ctx.result("You have deleted the rule : " + ctx.param("ruleName") + "\n");
 		});
-		
+
 		/*
 		 * Start the thread and make automatic flow gestion
-		 * */
-		serv.get("/mode-auto-on",  ctx ->{
+		 */
+		serv.get("/mode-auto-on", ctx -> {
 			FlowGestion.start1();
 			ctx.result("You set auto-mode on ! \n");
 		});
-		
+
 		/*
 		 * Stop the thread
 		 */
-		serv.get("/mode-auto-off", ctx ->{
+		serv.get("/mode-auto-off", ctx -> {
 			FlowGestion.interrupted();
 			ctx.result("You set auto-mode off ! \n");
 		});
